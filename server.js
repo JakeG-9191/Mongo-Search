dc0a629e143a1f038e950b3cfa3a7cec6502549f
Jake Garlick <jacobgarlick@msn.com>
2019-10-28 15:36:48 -0600

    added ignore, updated server js, included all required npm installs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var logger = require("morgan");
+var axios = require("axios");
+var cheerio = require("cheerio");
 var app = express();
 
 app.use(logger("dev"));
@@ -12,9 +14,34 @@ var databaseUrl = "webScrape";
 var collections = ["WashingtonPost"];
 
 var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
 });
+
+app.get("/api/all", function (req, res) {
+    db.scrapedData.find({}, function (err, data) {
+        if (err) return res.status(500).end();
+        res.json(data)
+    })
+})
+
+app.get("/scraped", function (req, res) {
+    axios.get("http://www.washingtonpost.com/")
+        .then(function (response) {
+            var $ = cheerio.load(response.data);
+
+            $("div.headline").each(function (i, element) {
+                var title = $(element).children().text();
+                var link = $(element).find("a").attr("href");
+
+                db.scrapedData.insert({
+                    "title": title,
+                    "link": link
+                })
+            });
+            res.send("Scrape Initiated")
+        })
+})
 
 app.listen(3000, function () {
     console.log("App running on port 3000!");
