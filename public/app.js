4b9f41779a8ffbd99d7d475f132aab42485cc7b3
Jake Garlick <jacobgarlick@msn.com>
2019-10-29 19:04:07 -0600

    added addiitonal functionality for users to add comment, comment will be displayed in new class on front end

---
+++ public/app.js
@@ -0,0 +1,29 @@
+import { on } from "cluster";
+
+$getJSON("/articles", function(data){
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
+
+$(".comment").on("click", function(){
+    $("#user-comment").empty();
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+    .then(function(data){
+        console.log(data);
+        $("#user-comment").append("<h3>" + data.title + "</h3>");
+        $("#user-comment").append("<input id='titleinput' name='title'>");
+        $("#user-comment").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#user-comment").append("<button data-id='" + data._id + "'id='savecomment>Save Your Comment</button>");
+
+        if(data.note){
+            $("#comment-title").val(data.comment.title);
+            $("#comment-body").val(data.comment.body)
+        }
+    });
+});
